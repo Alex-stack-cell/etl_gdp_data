e6a9f3e70de5b910d232fe0b6ff813f70b8c0e19
Alexandre <alexandregavriilidis@gmail.com>
2025-09-03 23:10:33 +0300

    feat: Extract data from the web page through an api call

--- etl_gdp.py
+++ etl_gdp.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import sqlite3
 import numpy
 from datetime import datetime
+from typing import List, Dict, Any
 
 csv_file = 'Countries_by_GDP.csv'
 db_table = 'Countries_by_GDP'
@@ -12,32 +13,91 @@ db_file = 'World_Economies.db'
 
 countries_gdp_attributes = ['Country', 'GDP_USD_billion']
 
-url = "'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'"
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
 
 ''' This function extracts the required
 information from the website and saves it to a dataframe. The
 function returns the dataframe for further processing. '''
-def extract(url: str, table_attribs: [str]) -> DataFrame:
-    pass
+
+
+def extract(url: str, table_attribs: List[str]) -> DataFrame:
+    df = DataFrame(columns=table_attribs)
+    try:
+        response = requests.get(url).text
+        html_page_data = BeautifulSoup(response, 'html.parser')
+        table_data = html_page_data.find_all('table', {'class': 'wikitable'})
+        table_body = table_data[0].find('tbody')
+        rows = table_body.find_all('tr')
+
+        for row in rows:
+            cols = row.find_all('td')
+            if (cols and  # Not an empty row
+                len(cols) > 2 and  # Has at least 3 columns
+                cols[0].find('a') and  # First column contains hyperlink
+                    cols[2].get_text(strip=True) != '—'):  # Third column not '—'
+
+                row_dict = {}
+                row_dict['Country'] = cols[0].get_text(
+                    strip=True)  # Column 0: Country
+                row_dict['GDP_USD_billion'] = cols[2].get_text(
+                    strip=True)  # Column 2: IMF Estimate
+
+                df = pd.concat([df, pd.DataFrame([row_dict])],
+                               ignore_index=True)
+
+    except requests.exceptions.HTTPError as http_error:
+        print(f"Http error : {http_error}")
+    except requests.exceptions.ConnectionError as conn_error:
+        print(f"Connection error: {conn_error}")
+    except requests.exceptions.Timeout as time_out:
+        print(f"Timeout error, {time_out}")
+    except requests.exceptions.RequestException as request_error:
+        print(f"Request exception, {request_error}")
+
+    finally:
+        return df
+
 
 ''' This function converts the GDP information from Currency
 format to float value, transforms the information of GDP from
 USD (Millions) to USD (Billions) rounding to 2 decimal places.
 The function returns the transformed dataframe.'''
+
+
 def transform(df: DataFrame) -> DataFrame:
     pass
+
+
 ''' This function saves the final dataframe as a `CSV` file 
 in the provided path. Function returns nothing.'''
-def load_to_csv(df: DataFrame, csv_path:str) -> None:
+
+
+def load_to_csv(df: DataFrame, csv_path: str) -> None:
     pass
+
+
 ''' This function saves the final dataframe as a database table
 with the provided name. Function returns nothing.'''
-def load_to_db(df: DataFrame, sql_connection:str, table_name:str) -> None:
+
+
+def load_to_db(df: DataFrame, sql_connection: str, table_name: str) -> None:
     pass
+
+
 ''' This function runs the stated query on the database table and
 prints the output on the terminal. Function returns nothing. '''
-def run_query(query_statement: str, sql_connection:str) ->None:
+
+
+def run_query(query_statement: str, sql_connection: str) -> None:
     pass
+
+
 ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
+
+
 def log_progress(message: str) -> None:
     pass
+
+
+df = extract(url, countries_gdp_attributes)
+print(df)
